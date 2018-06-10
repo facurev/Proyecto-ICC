d00d0b8ed0919766711a7e87c161d55a40d63152
Dalex98 <39716896+Dalex98@users.noreply.github.com>
2018-06-10 16:49:57 -0500

    Add files via upload

---
+++ candidatos.py
@@ -0,0 +1,18 @@
+candidatos=[]
+partidos=[]
+municipios=[]
+n=int(input())
+
+for i in range(n):
+    nombre = (input("Nombre del Postulante:"))
+    par = (input("Partido del Postulante:"))
+    muni = (input("Municipio del Postulante:"))
+
+    candidatos.append(nombre)
+    partidos.append(par)
+    municipios.append(muni)
+
+print(candidatos)
+print(partidos)
+print(municipios)
+
